104d40ca6ce32adc27558fdfb2b9b0207dcdc39e
superlover <126624921+superl0ver@users.noreply.github.com>
2023-03-10 22:31:39 +0000

    Complete E4

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ def main():
     # Comlete your code here
     ##################################################
  celcius = int(input('Enter the Celcius Degree: '))
- Fahrenheit = (9.0 / 5.0) * celcius + 32
-print(f'The Fahrenheit is {fahrenheit:.2f}')
+ fahrenheit = (9.0 / 5.0) * celcius + 32
+ print(f'The fahrenheit is {fahrenheit:.2f}')
 
-  pass
+   
+pass 
   
 
 
